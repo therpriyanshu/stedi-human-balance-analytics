cc0534c183f0779cbe816e591ee49f406a6749ad
Priyanshu Kumar <126187612+therpriyanshu@users.noreply.github.com>
2026-07-08 18:49:40 +0530

    Update customer_landing_to_trusted.py

--- customer_landing_to_trusted.py
+++ customer_landing_to_trusted.py
@@ -4,38 +4,32 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-import re
+from awsglue import DynamicFrame
 
-args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init(args["JOB_NAME"], args)
+job.init(args['JOB_NAME'], args)
 
-# Script generated for node Customer Landing
-CustomerLanding_node1 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={"paths": ["s3://stedi-s3/customer/landing/"], "recurse": True},
-    transformation_ctx="CustomerLanding_node1",
-)
+# Script generated for node AWS Glue Data Catalog
+AWSGlueDataCatalog_node1783516149811 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_landing", transformation_ctx="AWSGlueDataCatalog_node1783516149811")
 
-# Script generated for node Privacy Filter
-PrivacyFilter_node1693740592249 = Filter.apply(
-    frame=CustomerLanding_node1,
-    f=lambda row: (not (row["shareWithResearchAsOfDate"] == 0)),
-    transformation_ctx="PrivacyFilter_node1693740592249",
-)
+# Script generated for node SQL Query
+SqlQuery0 = '''
+SELECT *
+FROM myDataSource
+WHERE sharewithresearchasofdate IS NOT NULL
+'''
+SQLQuery_node1783516160934 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":AWSGlueDataCatalog_node1783516149811}, transformation_ctx = "SQLQuery_node1783516160934")
 
-# Script generated for node Customer Trusted
-CustomerTrusted_node3 = glueContext.write_dynamic_frame.from_options(
-    frame=PrivacyFilter_node1693740592249,
-    connection_type="s3",
-    format="json",
-    connection_options={"path": "s3://stedi-s3/customer/trusted/", "partitionKeys": []},
-    transformation_ctx="CustomerTrusted_node3",
-)
+# Script generated for node AWS Glue Data Catalog
+AWSGlueDataCatalog_node1783516206255 = glueContext.write_dynamic_frame.from_catalog(frame=SQLQuery_node1783516160934, database="stedi", table_name="customer_trusted", transformation_ctx="AWSGlueDataCatalog_node1783516206255")
 
 job.commit()
